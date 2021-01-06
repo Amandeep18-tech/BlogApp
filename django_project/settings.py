bd9a54e6c9aaccbf0770ed39df1232f9ea3dc92b
Amandeep <amandeep@yodaplus.com>
2021-01-06 17:59:56 +0530

    tutorial 10

--- django_project/settings.py
+++ django_project/settings.py
@@ -125,4 +125,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
 CRISPY_TEMPLATE_PACK= 'bootstrap4'
+LOGIN_REDIRECT_URL='blog-home'
+LOGIN_URL='login'
--- users/views.py
+++ users/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from .forms import ProfileUpdateForm
+from .forms import UserUpdateForm
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -11,8 +14,32 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Account created for {username}! You can now log in')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        user_form=UserUpdateForm(request.POST,instance=request.user)
+        profile_form=ProfileUpdateForm(request.POST,
+                                        request.FILES,
+                                        instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Account has been updated')
+            return redirect('profile')
+
+    else:
+        user_form=UserUpdateForm(instance=request.user)
+        profile_form=ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'user_form':user_form,
+        'profile_form':profile_form
+    }
+
+    return render(request,'profile.html',context)
